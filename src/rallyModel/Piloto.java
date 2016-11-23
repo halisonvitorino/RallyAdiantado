c7cd76ddbaa160047f5ade200ddcb5398d0f336e
halison.vitorino <halison.vitorino@halisonribeiro.foton.local>
2016-11-23 12:36:00 -0200

    Incluido classe pista.

--- src/rallyModel/Piloto.java
+++ src/rallyModel/Piloto.java
@@ -7,48 +7,33 @@ public class Piloto {
 	private int posicaoPiloto;
 	
 	
-
 	public int getPosicaoPiloto() {
 		return posicaoPiloto;
 	}
-
-
 
 	public void setPosicaoPiloto(int posicaoPiloto) {
 		this.posicaoPiloto = posicaoPiloto;
 	}
 
-
-
 	public int getIdPiloto() {
 		return idPiloto;
 	}
-
-
 
 	public void setIdPiloto(int idPiloto) {
 		this.idPiloto = idPiloto;
 	}
 
-
-
 	public String getNomePiloto() {
 		return nomePiloto;
 	}
-
-
 
 	public void setNomePiloto(String nomePiloto) {
 		this.nomePiloto = nomePiloto;
 	}
 
-
-
 	public Piloto() {
 	
 		// TODO Auto-generated constructor stub
 	}
-	
-	
 
 }
