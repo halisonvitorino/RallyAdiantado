64d70891209aab4e9f4c96f0c7b58a03baf08fae
halison.vitorino <halison.vitorino@halisonribeiro.foton.local>
2016-11-23 11:32:29 -0200

    adicionando piloto

--- src/rallyModel/Piloto.java
+++ src/rallyModel/Piloto.java
@@ -1,5 +1,41 @@
 package rallyModel;
 
 public class Piloto {
+	
+	private int idPiloto;
+	private String nomePiloto;
+	
+	
+
+	public int getIdPiloto() {
+		return idPiloto;
+	}
+
+
+
+	public void setIdPiloto(int idPiloto) {
+		this.idPiloto = idPiloto;
+	}
+
+
+
+	public String getNomePiloto() {
+		return nomePiloto;
+	}
+
+
+
+	public void setNomePiloto(String nomePiloto) {
+		this.nomePiloto = nomePiloto;
+	}
+
+
+
+	public Piloto() {
+	
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
